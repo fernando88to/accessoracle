51876b2f6d5244407d0ca7477fe16294aa35dd08
Fernando <fernando88to@gmail.com>
2022-04-02 20:35:54 -0300

    teste funcioando

--- src/main/java/com/fernando/controller/PessoaController.java
+++ src/main/java/com/fernando/controller/PessoaController.java
@@ -3,15 +3,21 @@ package com.fernando.controller;
 import com.fernando.domain.Pessoa;
 import com.fernando.repository.PessoaRepository;
 import com.fernando.services.PessoaService;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.panache.common.Sort;
+import org.hibernate.Criteria;
 
 import javax.inject.Inject;
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Path("/pessoa")
 @Produces(MediaType.APPLICATION_JSON)
@@ -26,6 +32,21 @@ public class PessoaController {
     @GET
     public List<Pessoa> getPessoas() {
         return Pessoa.listAll(Sort.by("id").descending());
+    }
+    @GET
+    @Path("/pesquisa")
+    public List<Pessoa> pesquisa() {
+        /*try (Stream<Pessoa> persons = Pessoa.streamAll()) {
+            List<Pessoa> namesButEmmanuels = persons
+                    .filter( n -> n.nome.equals("Fernando"))
+                    .filter( n -> n.id.equals(1l))
+                    .collect(Collectors.toList());
+            return namesButEmmanuels;
+        }*/
+
+        return pessoaRepository.consultar2();
+
+
     }
     @GET
     @Path("/list")
--- src/main/java/com/fernando/repository/PessoaRepository.java
+++ src/main/java/com/fernando/repository/PessoaRepository.java
@@ -4,13 +4,53 @@ import com.fernando.domain.Pessoa;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @ApplicationScoped
 public class PessoaRepository implements PanacheRepository<Pessoa> {
-
+    @PersistenceContext
+    EntityManager em;
     public List<Pessoa> consultar() {
         return listAll();
     }
+
+    /*public List<Pessoa> consultar2() {
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Pessoa> criteriaQuery = criteriaBuilder.createQuery(Pessoa.class);
+        Root<Pessoa> root = criteriaQuery.from(Pessoa.class);
+
+
+        return em.createQuery(criteriaQuery).getResultList();
+
+
+
+    }*/
+    public List<Pessoa> consultar2() {
+        CriteriaBuilder  criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Pessoa> criteriaQuery = criteriaBuilder.createQuery(Pessoa.class);
+        Root<Pessoa> studentRoot = criteriaQuery.from(Pessoa.class);
+        criteriaQuery.select(studentRoot);
+
+        //Predicate predicate = criteriaBuilder.equal(studentRoot.get("id"), 1l);
+        //Predicate predicate2 = criteriaBuilder.equal(studentRoot.get("nome"), "Fernando");
+        ///Predicate predicate = criteriaBuilder.like(studentRoot.get("nome"), "%E%");
+
+        Predicate predicate = criteriaBuilder.like(criteriaBuilder.upper(studentRoot.get("nome")),"%E%");
+        //criteriaQuery.where(predicate, predicate2);
+        criteriaQuery.where(predicate);
+        TypedQuery<Pessoa> typedQuery = em.createQuery(criteriaQuery);
+        List<Pessoa> studentList = typedQuery.getResultList();
+        return studentList;
+
+    }
+
+
 
 }
